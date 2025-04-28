90ef6d32e48db7cb78da8af22b30a961c65e9603
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-04-28 17:10:40 +0200

    Refactor

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { getEnvVar } from "../utils/getEnvVar";
+import getEnvVar from "../utils/getEnvVar";
 
 export interface UserRequest extends Request {
   userId?: string;
