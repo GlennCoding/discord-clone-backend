518ed6f001d9eaace88c9820b8bc40ff4e891d67
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-05-31 15:04:26 +0200

    wip

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://websocket-showcase-frontend.vercel.app",
+  "https://websocket-showcase-frontend-e2d39yglr-glenncodings-projects.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
