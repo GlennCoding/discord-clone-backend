3b4d3278b3dec39dc61d61ddd323ca0889bb29fb
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-06-03 15:10:36 +0200

    Add socket.io server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,10 @@ import express, {
   RequestHandler,
 } from "express";
 import cors from "cors";
+import { Server, Socket } from "socket.io";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { connectDB } from "./config/dbConn";
+
 import rootRouter from "./routes/root";
 import authRouter from "./routes/auth";
 import registerRouter from "./routes/register";
@@ -20,9 +23,14 @@ import cookieParser from "cookie-parser";
 import credentials from "./middleware/credentials";
 import corsOptions from "./config/corsOptions";
 import getEnvVar from "./utils/getEnvVar";
+import User from "./models/User";
 
-const PORT = getEnvVar("PORT") || 3000;
+const PORT = getEnvVar("PORT") || 8000;
 const app = express();
+
+const io = new Server({
+  cors: corsOptions,
+});
 
 connectDB();
 
@@ -31,6 +39,40 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
+
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.auth.token;
+
+    // Verify and decode the JWT
+    const decoded = jwt.verify(token, getEnvVar("ACCESS_TOKEN_SECRET"));
+
+    // Get the user information from the database
+    const user = await User.findById((decoded as JwtPayload).UserInfo.userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Attach the user object to the socket
+    socket.data.user = user;
+    next();
+  } catch (error) {
+    console.error("Authentication error", error);
+    next(new Error("Authentication error"));
+  }
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("Connected!");
+
+  socket.on("hello world", () => {
+    console.debug("hello world");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
 
 app.use("/", rootRouter);
 app.use("/register", registerRouter);
@@ -52,3 +94,5 @@ app.use((err: Errback, _req: Request, res: Response, next: NextFunction) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+io.listen(8001);
