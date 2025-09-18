76c03de8abf8229d5e8b7e677a2a7a281e8aff35
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-09-18 15:50:53 +0200

    ref: Improve socket file structure

--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Create event types for socket.io -> That know their callbacks
- *
- * Socket listeners:
- * - Get specific data
- * - Return specific data
- * - Return with success & error
- */
