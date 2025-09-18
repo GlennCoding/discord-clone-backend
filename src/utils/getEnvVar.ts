529c331a8193e781ce18975a97b70d248ac14248
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-18 16:44:19 +0200

    feat: added mvp env setup

--- src/utils/getEnvVar.ts
+++
@@ -1,9 +0,0 @@
-const getEnvVar = (key: string): string => {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Missing environment variable: ${key}`);
-  }
-  return value;
-};
-
-export default getEnvVar;
