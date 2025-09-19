1c658788605e407330355f8d5d003dea60e7f9e9
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-09-19 17:22:37 +0200

    ref: Add improved socket event typing

--- src/types/sockets.ts
+++ src/types/sockets.ts
@@ -1,12 +1,17 @@
-export type IMessageAPI = {
-  text: string;
-  chatId: string;
-  sender: "self" | "other";
-  createdAt: string;
-  id: string;
-};
+import { Server, Socket } from "socket.io";
+import { ClientToServerEvents, ServerToClientEvents } from "./events";
 
-export type IChatAPI = {
-  chatId: string;
-  participant: string;
-};
+export type TypedServer = Server<ClientToServerEvents, ServerToClientEvents>;
+
+export type TypedSocket = Socket<ClientToServerEvents, ServerToClientEvents>;
+
+type EventParams<T> = T extends (payload: infer P, ack: infer A) => void
+  ? [payload: P, ack?: A]
+  : T extends (payload: infer P) => void
+  ? [payload: P]
+  : [];
+
+export type EventController<E extends keyof ClientToServerEvents> = (
+  socket: TypedSocket,
+  ...args: EventParams<ClientToServerEvents[E]>
+) => Promise<void> | void;
