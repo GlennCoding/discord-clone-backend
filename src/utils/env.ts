a8400d9cdc5aac40a8484868bb9c1c36796dee7a
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-20 09:23:51 +0200

    refactor: abstracted constant

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
+const DEFAULT_PORT = 8000;
+
 const envSchema = z.object({
   DATABASE_URI: z.string(),
   DB_NAME: z.string(),
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
-  PORT: z.coerce.number().default(8000),
+  PORT: z.coerce.number().default(DEFAULT_PORT),
 });
 
 export const env = envSchema.parse(process.env);
