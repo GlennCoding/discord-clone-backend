6bd5e94971aff1857712513e75053aa995d005e7
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-20 09:22:38 +0200

    feat: added new way of handling env parsing

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,11 @@
-import { parseEnv } from "znv";
 import { z } from "zod";
 
-export const env = parseEnv(process.env, {
+const envSchema = z.object({
   DATABASE_URI: z.string(),
   DB_NAME: z.string(),
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   PORT: z.coerce.number().default(8000),
 });
+
+export const env = envSchema.parse(process.env);
