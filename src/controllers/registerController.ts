d515f1a06edc65848e8d8fe1b2048a4d98f59220
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-09-22 15:10:49 +0200

    ref: Seperate register router and controller

---
+++ src/controllers/registerController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import {
+  createUser,
+  findUserWithUserName,
+  saveUserRefreshToken,
+} from "../services/userService";
+import { issueAuthToken, issueRefreshToken } from "../services/authService";
+
+export const handleRegister = async (req: Request, res: Response) => {
+  const { userName, password } = req.body;
+
+  if (userName === undefined || password === undefined) {
+    res.status(400).json({ error: "username and password are required." });
+    return;
+  }
+
+  const usernameExistsAlready = await findUserWithUserName(userName);
+
+  if (usernameExistsAlready) {
+    res.status(409).json({ error: "Username already taken." });
+    return;
+  }
+
+  const user = await createUser(userName, password);
+
+  const accessToken = issueAuthToken(user);
+
+  const refreshToken = issueRefreshToken(user);
+
+  await saveUserRefreshToken(user, refreshToken);
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res
+    .status(201)
+    .json({ message: "User registered successfully.", token: accessToken });
+};
--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,28 +1,9 @@
-import { Router, Request, Response } from "express";
-import { createUser, findUserWithUserName } from "../services/userService";
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import { handleRegister } from "../controllers/registerController";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { userName, password } = req.body;
-
-  if (userName === undefined || password === undefined) {
-    res.status(400).json({ error: "username and password are required." });
-    return;
-  }
-
-  // Check if user already exists
-  const usernameExistsAlready = await findUserWithUserName(userName);
-
-  if (usernameExistsAlready) {
-    res.status(409).json({ error: "Username already taken." });
-    return;
-  }
-
-  // Create and save new user
-  await createUser(userName, password);
-
-  res.status(201).json({ message: "User registered successfully." });
-});
+router.post("/", asyncHandler(handleRegister));
 
 export default router;
