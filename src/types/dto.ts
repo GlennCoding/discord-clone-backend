9193c81feff6190e17cc0617cad0d9334ca4d14c
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-09-22 16:06:02 +0200

    ref: Update DTO and Input types

---
+++ src/types/dto.ts
@@ -0,0 +1,22 @@
+export interface MessageDTO {
+  text: string;
+  chatId: string;
+  sender: "self" | "other";
+  createdAt: string;
+  id: string;
+}
+
+export interface ChatDTO {
+  chatId: string;
+  participant: string;
+}
+
+export interface JoinChatDTO {
+  participant: string;
+  messages: MessageDTO[];
+}
+
+export interface SendMessageInput {
+  chatId: string;
+  text: string;
+}
--- src/types/events.ts
+++ src/types/events.ts
@@ -1,31 +1,10 @@
+import { SendMessageInput, MessageDTO, JoinChatDTO } from "./dto";
+
 export class EVENT_ERROR {
   constructor({ error, message }: { error: ERROR_STATUS; message: string }) {
     this.error = error;
     this.message = message;
   }
-}
-
-export interface MessageDTO {
-  text: string;
-  chatId: string;
-  sender: "self" | "other";
-  createdAt: string;
-  id: string;
-}
-
-export interface ChatDTO {
-  chatId: string;
-  participant: string;
-}
-
-export interface JoinChatResponse {
-  participant: string;
-  messages: MessageDTO[];
-}
-
-export interface SendMessagePayload {
-  chatId: string;
-  text: string;
 }
 
 export enum ERROR_STATUS {
@@ -48,10 +27,10 @@ type Ack<T> = (res: EVENT_SUCCESS<T> | EVENT_ERROR) => void;
 
 export interface ClientToServerEvents {
   "message:send": (
-    payload: SendMessagePayload,
+    payload: SendMessageInput,
     ack: Ack<{ message: MessageDTO }>
   ) => void;
-  "chat:join": (chatId: string, ack: Ack<JoinChatResponse>) => void;
+  "chat:join": (chatId: string, ack: Ack<JoinChatDTO>) => void;
   "chat:leave": (chatId: string) => void;
 }
 
