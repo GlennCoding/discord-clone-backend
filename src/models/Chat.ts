55f396e44a92218d7d23c38c9612e968a5f4dfb0
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-11-01 18:24:49 +0100

    feat: add timestamps to Chat and Reaction models

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -4,22 +4,27 @@ import { IUser } from "./User";
 export interface IChat extends Document {
   _id: Types.ObjectId;
   participants: (Types.ObjectId | IUser)[];
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 const arrayLimit = (val: String[]) => {
   return val.length === 2;
 };
 
-const chatSchema = new Schema<IChat>({
-  participants: {
-    type: [Schema.Types.ObjectId],
-    ref: "User",
-    required: true,
-    validate: {
-      validator: arrayLimit,
-      message: "A conversation must have exactly 2 participants.",
+const chatSchema = new Schema<IChat>(
+  {
+    participants: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+      required: true,
+      validate: {
+        validator: arrayLimit,
+        message: "A conversation must have exactly 2 participants.",
+      },
     },
   },
-});
+  { timestamps: true }
+);
 
 export default model("Chat", chatSchema);
--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -5,12 +5,17 @@ export interface IReaction extends Document {
   message: Types.ObjectId;
   emoji: string;
   sender: Types.ObjectId;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
-const reactionSchema = new Schema<IReaction>({
-  message: { type: Schema.Types.ObjectId, ref: "Message", required: true },
-  sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  emoji: { type: String, required: true },
-});
+const reactionSchema = new Schema<IReaction>(
+  {
+    message: { type: Schema.Types.ObjectId, ref: "Message", required: true },
+    sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    emoji: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export default model("User", reactionSchema);
