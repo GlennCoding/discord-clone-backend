3f507189e51823f0404299642160833a0d46856f
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-11-03 17:35:13 +0100

    feat: Implement getAllPublicServers controller

--- src/routes/api/servers.ts
+++ src/routes/api/servers.ts
@@ -4,6 +4,7 @@ import {
   createServer,
   updateServer,
   deleteServer,
+  getAllPublicServers,
 } from "../../controllers/serverController";
 
 const router = Router();
@@ -12,7 +13,7 @@ router.post("/", asyncHandler(createServer));
 router.put("/:id", asyncHandler(updateServer));
 router.delete("/:id", asyncHandler(deleteServer));
 
-// router.get("/public", asyncHandler());
+router.get("/public", asyncHandler(getAllPublicServers));
 // router.get("/joined", asyncHandler());
 // router.get("/:shortId");
 // router.post("/:shortId/join", asyncHandler());
