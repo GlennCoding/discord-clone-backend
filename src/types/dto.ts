28146dc8eac7dcddd19837dbae80efb3544fbf48
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2025-11-18 15:40:44 +0100

    feat: Implement channelMessageHandlers

--- src/types/dto.ts
+++ src/types/dto.ts
@@ -135,7 +135,8 @@ export interface ChannelMessageDTO {
 }
 
 export interface ChannelSubscribeDTO {
-  channel: Omit<ChannelDTO, "order">;
+  serverId: string;
+  channel: ChannelDTO;
   messages: ChannelMessageDTO[];
 }
 
