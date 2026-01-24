b0c811497637c391126b92c0f7e869189c6abdfb
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-24 15:32:24 +0100

    ref: implement userRepository in chatMessageService

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,8 @@
 import MongooseChatMessageRepository from "./repositories/mongooseChatMessageRepository";
+import MongooseUserRepository from "./repositories/mongooseUserRepository";
 import ChatMessageService from "./services/chatMessageService";
 
+const userRepo = new MongooseUserRepository();
 const chatMessageRepo = new MongooseChatMessageRepository();
 
-export const chatMessageService = new ChatMessageService(chatMessageRepo);
+export const chatMessageService = new ChatMessageService(userRepo, chatMessageRepo);
