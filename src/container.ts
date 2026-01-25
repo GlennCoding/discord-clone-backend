0d195dfee299619361af22db5b63de9286796dcd
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-25 16:32:40 +0100

    feat: wirte bucket into fileStorage instance in container.ts

--- src/container.ts
+++ src/container.ts
@@ -1,14 +1,18 @@
+import { bucket } from "./config/storage";
 import { GcsFileStorage } from "./infrastructure/GcsFileStorage";
 import MongooseChatMessageRepository from "./repositories/mongooseChatMessageRepository";
+import MongooseChatRepository from "./repositories/mongooseChatRepository";
 import MongooseUserRepository from "./repositories/mongooseUserRepository";
 import ChatMessageService from "./services/chatMessageService";
 
 const userRepo = new MongooseUserRepository();
+const chatRepo = new MongooseChatRepository();
 const chatMessageRepo = new MongooseChatMessageRepository();
-const fileStorage = new GcsFileStorage();
+const fileStorage = new GcsFileStorage(bucket);
 
 export const chatMessageService = new ChatMessageService(
   userRepo,
+  chatRepo,
   chatMessageRepo,
   fileStorage,
 );
