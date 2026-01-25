2756103464b184110757c9cf7eeb5fc0ad138372
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-25 17:48:34 +0100

    ref: implement ChatMessageAttachmentService in messageController

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,24 +1,10 @@
 import { UserRequest } from "../middleware/verifyJWT";
 import { Response } from "express";
-import {
-  CustomError,
-  InputMissingError,
-  ParamsMissingError,
-  UserNotFoundError,
-} from "../utils/errors";
-import { findUserWithUserId } from "../services/userService";
-import {
-  deleteFileFromBucket,
-  uploadFileToBucket,
-} from "../services/storageService";
-import Message from "../models/ChatMessage";
+import { InputMissingError } from "../utils/errors";
 import {
   DeleteMessageAttachmentInput,
   SaveMessageAttachmentInput,
 } from "../types/dto";
-import Chat from "../models/Chat";
-import mongoose from "mongoose";
-import { idsEqual } from "../utils/helper";
 import { io } from "../app";
 import { toMessageDTO } from "../utils/dtos/messageDTO";
 import {
@@ -26,32 +12,10 @@ import {
   MAX_MESSAGE_ATTACHMENT_FILE_SIZE_BYTES,
 } from "../config/upload";
 import { validateUploadedFile } from "../utils/fileValidation";
-import { buildObjectKey } from "../utils/storage";
 import { auditHttp } from "../utils/audit";
 import z from "zod";
 import { parseWithSchema } from "../utils/validators";
 import { chatMessageAttachmentService } from "../container";
-
-const getChat = async (chatId: string | undefined, userId: string) => {
-  // is user part of chat? -> Chat.find()
-  const foundChat = await Chat.findOne({ _id: chatId });
-  if (!foundChat) throw new CustomError(400, "Chat doesn't exist");
-
-  const userIsParticipant = foundChat.participants.some((participant) =>
-    idsEqual(participant, new mongoose.Types.ObjectId(userId)),
-  );
-
-  if (!userIsParticipant) {
-    throw new CustomError(403, "User is not part of this chat");
-  }
-
-  return foundChat;
-};
-
-const verifyText = (text?: string) => {
-  if (!text) return;
-  if (typeof text !== "string") throw new CustomError(400, "text must be string");
-};
 
 const saveMessageAttachmentPayloadSchema = z.object({
   chatId: z.string(),
@@ -62,57 +26,32 @@ export const saveMessageAttachment = async (
   req: UserRequest<SaveMessageAttachmentInput>,
   res: Response,
 ) => {
-  const { file, userId } = req;
-
   const { chatId, text } = parseWithSchema(
     saveMessageAttachmentPayloadSchema,
     req.body,
   );
-  if (!file) throw new InputMissingError("File");
 
-  const user = await findUserWithUserId(req.userId as string);
-  if (!user) throw new UserNotFoundError();
-
-  if (!chatId) throw new ParamsMissingError("chat id");
-  const chat = await getChat(chatId, userId as string);
-
-  verifyText(text);
-
-  const validatedFile = await validateUploadedFile(file, {
+  if (!req.file) throw new InputMissingError("File");
+  await validateUploadedFile(req.file, {
     allowedMimeTypes: ALLOWED_MESSAGE_ATTACHMENT_MIME_TYPES,
     maxFileSizeBytes: MAX_MESSAGE_ATTACHMENT_FILE_SIZE_BYTES,
   });
 
-  // upload file
-  const fileName = buildObjectKey(
-    "message-attachment",
-    user.id ?? user._id.toString(),
-    validatedFile.ext,
-  );
-  const downloadUrl = await uploadFileToBucket(file, fileName, validatedFile.mime);
-
-  // create & save Message document (if fails, delete file from bucket)
-  const newMessage = new Message({
-    chat: chat,
-    sender: user,
-    text: text,
-    attachments: [{ path: fileName, downloadUrl }],
+  const message = await chatMessageAttachmentService.saveMessageAttachment({
+    userId: req.userId as string,
+    file: req.file,
+    chatId,
+    text,
   });
 
-  try {
-    await newMessage.save();
-  } catch (err) {
-    console.warn(err);
-    deleteFileFromBucket(fileName);
-  }
+  auditHttp(req, "MESSAGE_ATTACHMENT_UPLOADED", { messageId: message.id });
 
-  const messageDTO = toMessageDTO(newMessage);
+  const messageDTO = toMessageDTO(message);
 
-  auditHttp(req, "MESSAGE_ATTACHMENT_UPLOADED", { messageId: newMessage.id });
-
-  io.to(chat.id).emit("message:new", {
+  io.to(chatId).emit("message:new", {
     message: messageDTO,
   });
+
   res.status(200).json({ message: messageDTO });
 };
 
