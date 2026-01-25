267a2b084812857aef937629935889e5fb6b836f
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-25 17:48:02 +0100

    ref: update toMessageDto to map from entity to dto

--- src/utils/dtos/messageDTO.ts
+++ src/utils/dtos/messageDTO.ts
@@ -1,16 +1,19 @@
 import { MessageDTO } from "../../types/dto";
-import { PopulatedMessage } from "../../types/misc";
+import { ChatMessageEntity } from "../../types/entities";
 
-export const toMessageDTO = (m: PopulatedMessage): MessageDTO => ({
-  id: m._id.toString(),
-  chatId: m.chat._id?.toString?.() ?? m.chat.toString(),
-  text: m.text,
-  sender: {
-    id: m.sender._id.toString(),
-    username: m.sender.userName,
-    avatarUrl: m.sender.avatar?.url,
-  },
-  createdAt: m.createdAt.toISOString(),
-  updatedAt: m.updatedAt?.toISOString(),
-  attachments: m.attachments?.map((a) => ({ downloadUrl: a.downloadUrl })) ?? [],
-});
+export const toMessageDTO = (message: ChatMessageEntity): MessageDTO => {
+  const { id, chatId, sender, text, attachments, createdAt, updatedAt } = message;
+  return {
+    id,
+    chatId,
+    text,
+    sender: {
+      id: sender.id,
+      username: sender.username,
+      avatarUrl: sender.avatarUrl,
+    },
+    createdAt: createdAt.toISOString(),
+    updatedAt: updatedAt?.toISOString(),
+    attachments: attachments?.map((a) => ({ downloadUrl: a.downloadUrl })) ?? [],
+  };
+};
