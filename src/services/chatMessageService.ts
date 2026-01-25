22663a4ff7379b3f948edbaaeba9c8041f6251b3
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-25 15:38:58 +0100

    ref: implement ChatMessageRepository create method in saveMessageAttachment service

--- src/services/chatMessageService.ts
+++ src/services/chatMessageService.ts
@@ -1,10 +1,27 @@
 import { FileStorage } from "../infrastructure/FileStorage";
 import { ChatMessageRepository } from "../repositories/chatMessageRepository";
 import { UserRepository } from "../repositories/userRepository";
-import { UserNotFoundError, NotFoundError, ForbiddenError } from "../utils/errors";
+import {
+  UserNotFoundError,
+  NotFoundError,
+  ForbiddenError,
+  CustomError,
+} from "../utils/errors";
 import { idsEqual } from "../utils/helper";
+import { ChatMessageEntity } from "../types/entities";
+import { buildObjectKey } from "../utils/storagePaths";
+import Message from "../models/ChatMessage";
+import mongoose from "mongoose";
+import Chat from "../models/Chat";
+import { uploadFileToBucket, deleteFileFromBucket } from "./storageService";
 
 interface IChatMessageService {
+  saveMessageAttachment: (input: {
+    userId: string;
+    file: Express.Multer.File;
+    chatId: string;
+    text: string | undefined;
+  }) => Promise<ChatMessageEntity>;
   deleteChatMessageAttachment: (input: {
     userId: string;
     messageId: string;
@@ -12,12 +29,62 @@ interface IChatMessageService {
   }) => Promise<void>;
 }
 
+const getChat = async (chatId: string | undefined, userId: string) => {
+  // is user part of chat? -> Chat.find()
+  const foundChat = await Chat.findOne({ _id: chatId });
+  if (!foundChat) throw new CustomError(400, "Chat doesn't exist");
+
+  const userIsParticipant = foundChat.participants.some((participant) =>
+    idsEqual(participant, new mongoose.Types.ObjectId(userId)),
+  );
+
+  if (!userIsParticipant) {
+    throw new CustomError(403, "User is not part of this chat");
+  }
+
+  return foundChat;
+};
+
 class ChatMessageService implements IChatMessageService {
   constructor(
     private user: UserRepository,
-    private chatMessages: ChatMessageRepository,
+    private chatMessage: ChatMessageRepository,
     private fileStorage: FileStorage,
   ) {}
+
+  async saveMessageAttachment(input: {
+    userId: string;
+    file: Express.Multer.File;
+    chatId: string;
+    text: string | undefined;
+  }) {
+    const { userId, file, chatId, text } = input;
+
+    const user = await this.user.findById(userId);
+    if (!user) throw new UserNotFoundError();
+
+    const chat = await getChat(chatId, userId);
+
+    // upload file
+    const fileName = buildObjectKey("message-attachment", user.id, file.mimetype);
+    const downloadUrl = await uploadFileToBucket(file, fileName, file.mimetype);
+
+    let newMessage: ChatMessageEntity;
+
+    try {
+      newMessage = await this.chatMessage.create({
+        chatId: chat._id.toString(),
+        senderId: userId,
+        text,
+        attachments: [{ path: fileName, downloadUrl }],
+      });
+    } catch (err) {
+      deleteFileFromBucket(fileName);
+      throw err;
+    }
+
+    return newMessage;
+  }
 
   async deleteChatMessageAttachment(input: {
     userId: string;
@@ -29,7 +96,7 @@ class ChatMessageService implements IChatMessageService {
     const user = this.user.findById(userId);
     if (!user) throw new UserNotFoundError();
 
-    const message = await this.chatMessages.findById(messageId);
+    const message = await this.chatMessage.findById(messageId);
     if (!message) throw new NotFoundError("Message");
 
     if (!idsEqual(message.senderId, userId))
@@ -49,9 +116,9 @@ class ChatMessageService implements IChatMessageService {
     );
 
     if (!message.text && newAttachments.length === 0) {
-      await this.chatMessages.deleteById(messageId);
+      await this.chatMessage.deleteById(messageId);
     } else {
-      await this.chatMessages.updateAttachments(messageId, newAttachments);
+      await this.chatMessage.updateAttachments(messageId, newAttachments);
     }
   }
 }
