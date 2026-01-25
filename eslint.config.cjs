20b8a9e3d9f8da1f8b97ca9eaa654ba2dea60797
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-25 20:03:53 +0100

    chore: add eslint to project

---
+++ eslint.config.cjs
@@ -0,0 +1,68 @@
+const path = require("path");
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+
+module.exports = [
+  {
+    ignores: ["node_modules", "dist", ".pnpm-store"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+        tsconfigRootDir: __dirname,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.es2022,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports" },
+      ],
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/require-await": "warn",
+    },
+  },
+  {
+    files: ["**/*.test.ts", "**/__tests__/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+        tsconfigRootDir: __dirname,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.es2022,
+        ...globals.node,
+        vi: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        beforeEach: "readonly",
+        afterAll: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+    },
+  },
+];
