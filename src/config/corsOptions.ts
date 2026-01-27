6c264a3964bfe21b63a6de5ab3ddec0d657ed6a9
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-27 14:40:22 +0100

    chore: tigthen cors options to include all methods

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,17 +2,16 @@ import allowedOrigins from "./allowedOrigins";
 
 import type { CorsOptions } from "cors";
 
-
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) return callback(null, true);
+
+    return callback(new Error("Not allowed by CORS"));
   },
   optionsSuccessStatus: 200,
   credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
 };
 
 export default corsOptions;
