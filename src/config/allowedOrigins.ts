8813750c040c0e61ae928002d12167ea637a7c8a
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-27 16:22:22 +0100

    fix: move local url in allowedOrigins from prod to non-prod environment

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -5,11 +5,10 @@ const allowedOrigins = [
   "https://discordclone.de",
   "https://epic-discord-clone.vercel.app",
   "https://discord-clone-frontend-glenncodings-projects.vercel.app",
-  "http://127.0.0.1:5500",
 ];
 
 if (!isProdEnv) {
-  allowedOrigins.push("http://localhost:3000", "http://localhost:3500");
+  allowedOrigins.push("http://localhost:3000", "http://localhost:3500", "http://127.0.0.1:5500");
 }
 
 export default allowedOrigins;
