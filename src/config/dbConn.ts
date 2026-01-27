fa3d91a5639daefa1936ef6d144442d82deb8b95
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2026-01-27 17:12:21 +0100

    feat: add mongoose timeoutMs

--- src/config/dbConn.ts
+++ src/config/dbConn.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 import { env } from "../utils/env";
 
+export const DEFAULT_QUERY_MAX_TIME_MS = 10_000; // 10s
+
 mongoose.set("strict", true);
 mongoose.set("strictQuery", true);
 mongoose.set("sanitizeFilter", true);
@@ -10,6 +12,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(env.DATABASE_URI as string, {
       dbName: env.DB_NAME as string,
+      timeoutMS: DEFAULT_QUERY_MAX_TIME_MS,
     });
     console.log("DB connection successful");
   } catch (error) {
